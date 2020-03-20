553f2f4aaf4b691f1600570b44b13fbca93609c2
Grant Kang <3s.grantkang@gmail.com>
2020-03-20 11:59:00 -0700

    Added the ajax call to create grades

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var apiKey = apiKeyResponse.apiKey;
+var username = "Grant Kang";
 
 function getAllGrades() {
   $.ajax({
@@ -14,6 +15,32 @@ function getAllGrades() {
       console.log(data);
     },
     error: function(err) {
+      console.log(err);
+    }
+  });
+}
+
+function createNewGrade(id,name,course,grade,createdBy) {
+  $.ajax({
+    method: "POST",
+    url: "https://sgt.lfzprototypes.com/api/grades",
+    headers: {
+      "X-Access-Token": apiKey
+    },
+    data: {
+      "id": id,
+      "name": name,
+      "course": course,
+      "grade": grade,
+      "createdBy": createdBy
+    },
+    complete: function () {
+      console.log("createNewGrade request completed");
+    },
+    success: function (data) {
+      console.log(data);
+    },
+    error: function (err) {
       console.log(err);
     }
   });
