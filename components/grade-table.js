49a320181d4a7e10313ee86f3d0669bc2c328e65
Grant Kang <3s.grantkang@gmail.com>
2020-03-21 16:36:17 -0700

    Enhanced grade table

--- components/grade-table.js
+++ components/grade-table.js
@@ -8,7 +8,7 @@ class GradeTable {
     var tbodyEl = this.tableElement.querySelector('tbody');
     tbodyEl.innerHTML = '';
     for(var data of grades) {
-      tbodyEl.appendChild(this.renderGradeRow(data, this.deleteGrade));
+      tbodyEl.appendChild(this.renderGradeRow(data, this.deleteGrade, this.toggleEdit));
     }
     if(!grades.length) {
       this.noGradesElement.classList.remove('d-none');
@@ -19,7 +19,10 @@ class GradeTable {
   onDeleteClick(deleteGrade) {
     this.deleteGrade = deleteGrade;
   }
-  renderGradeRow(data,deleteGrade) {
+  onEditClick(toggleEdit) {
+    this.toggleEdit = toggleEdit;
+  }
+  renderGradeRow(data,deleteGrade,toggleEdit) {
     var tableRowElement = document.createElement('tr');
 
     var tdNameElement = document.createElement('td');
@@ -32,10 +35,19 @@ class GradeTable {
     tdGradeElement.textContent = data.grade;
 
     var tdOperationsElement = document.createElement('td');
+
+    var editButton = document.createElement('button');
+    editButton.classList.add('btn','btn-primary');
+    editButton.textContent = "Edit";
+    editButton.addEventListener('click', function() {
+      toggleEdit(data);
+    });
+    tdOperationsElement.appendChild(editButton);
+
     var deleteButton = document.createElement('button');
-    deleteButton.classList.add('btn','btn-danger');
+    deleteButton.classList.add('btn', 'btn-danger');
     deleteButton.textContent = "Delete";
-    deleteButton.addEventListener('click', function() {
+    deleteButton.addEventListener('click', function () {
       deleteGrade(data.id);
     });
     tdOperationsElement.appendChild(deleteButton);
